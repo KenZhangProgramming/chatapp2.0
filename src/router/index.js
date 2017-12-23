689d7d898d8da189333113b2d7966cee95b67c60
kenzhang <kenverygood513@gmail.com>
2017-12-23 11:17:52 -0800

    set up routes and basic pages

--- src/router/index.js
+++ src/router/index.js
@@ -1,15 +1,9 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import form from '@/components/form'
+import Routes from '../routes'
 
 Vue.use(Router)
 
 export default new Router({
-  routes: [
-    {
-      path: '/',
-      name: 'form',
-      component: form
-    }
-  ]
+  routes: Routes
 })
---
+++ src/routes.js
@@ -0,0 +1,7 @@
+import Login from './components/login.vue'
+import Register from './components/register.vue'
+
+export default [
+  {path: '/', component: Login},
+  {path: '/register', component: Register}
+]
